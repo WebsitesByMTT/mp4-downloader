076856363c594629984b7c50628e07459b0360fa
TrippyGaurav <gaurav.trippybug@gmail.com>
2023-10-14 16:16:54 +0530

    testing 1

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,12 +5,12 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "YouTube to MP4 Downloader: Convert YouTube to MP4 instantly  ",
+  title: "YouTube to MP4 Downloader: Convert YouTube to MP4 instantly",
   description:
     "YouTube to MP4 Downloader makes it easy to watch videos offline. Now download YouTube video for free using this video converter online tool.",
 
   openGraph: {
-    title: "YouTube to MP4 Downloader: Convert YouTube to MP4 instantly  ",
+    title: "YouTube to MP4 Downloader: Convert YouTube to MP4 instantly",
     description:
       "YouTube to MP4 Downloader makes it easy to watch videos offline. Now download YouTube video for free using this video converter online tool.",
     url: "https://www.youtubetomp4downloader.com/",
